0294f1792461cdbfea293a8e173efe803f4070a2
alvaro garcia <derecerca@gmail.com>
2015-01-10 13:31:37 +0100

    green: add default src, test folders

---
+++ test/rpn_calculator/core_test.clj
@@ -0,0 +1,7 @@
+(ns rpn-calculator.core-test
+  (:require [clojure.test :refer :all]
+            [rpn-calculator.core :refer :all]))
+
+(deftest a-test
+  (testing "canary test"
+    (is (= 0 0))))
