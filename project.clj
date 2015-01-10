ea17c10c9720fdd39c13abd64800fe12bdad0558
alvaro garcia <derecerca@gmail.com>
2015-01-10 13:30:37 +0100

    add lein project with midje dependencies

---
+++ project.clj
@@ -0,0 +1,17 @@
+(defproject rpn-calculator "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "GNU GPL V3"
+            :url "http://www.gnu.org/licenses/gpl-3.0.txt"}
+  :dependencies [
+                 [org.clojure/clojure "1.6.0"]
+                 ]
+  :main ^:skip-aot rpn-calculator.core
+  :target-path "target/%s"
+  :profiles {  :uberjar {:aot :all}
+               :dev {
+                     :dependencies [[midje "1.6.3"]]
+                     :plugins      [[lein-midje "3.1.3"]]
+               }
+            }
+)
