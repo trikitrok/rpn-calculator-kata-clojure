185d39573c66e515bfac801f6f37d3a38d11d189
alvaro garcia <derecerca@gmail.com>
2015-01-10 13:44:24 +0100

    green: canary test using midje;
    run with 'lein midje :autotest'

--- test/rpn_calculator/core_test.clj
+++ test/rpn_calculator/core_test.clj
@@ -1,7 +1,15 @@
 (ns rpn-calculator.core-test
   (:require [clojure.test :refer :all]
-            [rpn-calculator.core :refer :all]))
+            [rpn-calculator.core :refer :all])
+  (:use [midje.sweet]))
+
 
 (deftest a-test
   (testing "canary test"
     (is (= 0 0))))
+
+(fact
+   "canary test in midje"
+   (= 0 0) => true
+ )
+
