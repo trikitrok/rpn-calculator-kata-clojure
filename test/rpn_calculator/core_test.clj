f0f09820f811708d7489042dc8df550cae629db0
Alvaro Garcia <derecerca@gmail.com>
2016-06-29 21:04:04 +0200

    refactor: whitespace

--- test/rpn_calculator/core_test.clj
+++ test/rpn_calculator/core_test.clj
@@ -5,36 +5,42 @@
 
 
 (facts
- "about the RPN calculator"
+  "about the RPN calculator"
 
   (fact
-   "0"
+    "0"
 
-   (calculate "0") => 0)
+    (calculate "0") => 0)
 
-   (fact
+  (fact
     "1"
-   (calculate "1") => 1)
+    (calculate "1") => 1)
 
- (fact "it should add numbers"
-       (calculate "1 1 +") => 2)
-
-
- (fact "it should add any number"
-       (calculate "0 1 +") => 1)
+  (fact
+    "it should add numbers"
+    (calculate "1 1 +") => 2)
 
 
- (fact "it should subtract numbers"
-       (calculate "3 2 -") => 1)
+  (fact
+    "it should add any number"
+    (calculate "0 1 +") => 1)
 
- (fact "it should add three numbers"
-       (calculate "1 2 3 + +") => 6)
 
- (fact "it should not accept wrong input"
-       (calculate "1 2 3 ' +") => nil
-       (calculate "'") => nil)
+  (fact
+    "it should subtract numbers"
+    (calculate "3 2 -") => 1)
 
- (fact "it should not accept empty expressions"
-       (calculate "") => nil
-       (calculate " ") => nil)
- )
+  (fact
+    "it should add three numbers"
+    (calculate "1 2 3 + +") => 6)
+
+  (fact
+    "it should not accept wrong input"
+    (calculate "1 2 3 ' +") => nil
+    (calculate "'") => nil)
+
+  (fact
+    "it should not accept empty expressions"
+    (calculate "") => nil
+    (calculate " ") => nil)
+  )
