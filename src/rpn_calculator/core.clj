ea14f427b5cb47626b730b94f5e2070e38a20da3
Alvaro Garcia <derecerca@gmail.com>
2016-06-29 11:34:09 +0200

    refactor: narrow exception to the thrown one

--- src/rpn_calculator/core.clj
+++ src/rpn_calculator/core.clj
@@ -40,7 +40,7 @@
 (defn parse-token [token]
   (try
     (Integer/parseInt token)
-    (catch Exception _
+    (catch NumberFormatException _
       (let [operators {"+" + "-" - "*" * "/" quot}]
         (if-let [op (get operators token)]
           op)))))
