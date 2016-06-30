b2bb993ffa917f1453b3cee4177f1a3d9c2e9d61
trikitrok <trikitrok@gmail.com>
2016-06-30 14:13:20 +0200

    format code

--- project.clj
+++ project.clj
@@ -1,18 +1,12 @@
 (defproject rpn-calculator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Revisiting RPN calculator kata in Clojure"
   :license {:name "GNU GPL V3"
             :url "http://www.gnu.org/licenses/gpl-3.0.txt"}
-  :dependencies [
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.logging "0.3.1"]
-                 ]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/tools.logging "0.3.1"]]
   :main ^:skip-aot rpn-calculator.core
   :target-path "target/%s"
-  :profiles {  :uberjar {:aot :all}
-               :dev {
-                     :dependencies [[midje "1.6.3"]]
-                     :plugins      [[lein-midje "3.1.3"]]
-               }
-            }
-)
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[midje "1.6.3"]]
+                   :plugins [[lein-midje "3.1.3"]]}
+             })
